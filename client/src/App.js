3047522e3ee3ba7297f846649e9838078b63154d
Chris <hello@hiimchrislim.co>
2022-01-26 19:59:53 -0500

    Set executable pre-commit file and add eslint for jest

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const App = () => {
-    return (
-        <div>
-            UTimeManager
-        </div>
-    );
-}
+  return <div>UTimeManager</div>;
+};
 
 export default App;
