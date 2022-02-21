5edea0545d2909f6c6c2ad02f74008590629faa8
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-02-21 00:02:07 -0500

    removed name value requirement from register route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,11 +2,11 @@ const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/jsonUtils");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
   console.log("HERE");
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
   const userExists = await User.findOne({ email });
 
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name: name,
     email: email,
-    password: password
+    password: password,
   });
 
   if (user) {
@@ -35,27 +35,37 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const user = await User.findOne({email: req.body.email});
+  const user = await User.findOne({ email: req.body.email });
   console.log(user);
-  if(!user) {
-      res.status(400);
-      throw new Error("No user found");
+  if (!user) {
+    res.status(400);
+    throw new Error("No user found");
   }
 
   const validate = await bcrypt.compare(req.body.password, user.password);
   console.log(validate);
-  if(!validate) {
-      res.status(400);
-      throw new Error("Wrong password");
+  if (!validate) {
+    res.status(400);
+    throw new Error("Wrong password");
   }
 
-  const {password, ...others} = user._doc;
+  const { password, ...others } = user._doc;
 
   //TODO: Return a cookie? How do you return a cookie?
-  res.status(200).json({token: jwt.sign({email: user.email, name: user.name, isAdmin: user.isAdmin, _id: user._id, }, process.env.JWT_SECRET, {
-          expiresIn: "12000s"
+  res.status(200).json({
+    token: jwt.sign(
+      {
+        email: user.email,
+        name: user.name,
+        isAdmin: user.isAdmin,
+        _id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "12000s",
       }
-  )});
-})
+    ),
+  });
+});
 
 module.exports = { registerUser, loginUser };
