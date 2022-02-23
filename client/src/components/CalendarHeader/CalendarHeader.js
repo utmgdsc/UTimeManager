6a88463e639d6eae468f1e669e39bb4125ed2c02
Vallens Kho <secure.mail2357@gmail.com>
2022-02-23 01:48:42 -0500

    Implement gradient background and date header

---
+++ client/src/components/CalendarHeader/CalendarHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ThemeText from "../ThemeText/ThemeText.js";
+
+const CalendarHeader = () => {
+  // TODO : create hamburger icon
+  return (
+    <div>
+      <ThemeText primary={true} text={"Daily Tasks"} />
+    </div>
+  );
+};
+
+export default CalendarHeader;
--- client/src/pages/CalendarPage/CalendarPage.js
+++ client/src/pages/CalendarPage/CalendarPage.js
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
 import "react-calendar/dist/Calendar.css";
-import "./CalendarPage.css";
+import "./CalendarOverride.css";
 import Calendar from "react-calendar";
+import styles from "./CalendarPage.module.css";
 
 const dayLabelFormatter = (locale, label) => {
   return label.toString().slice(0, 1);
+};
+
+const dailyTaskDateFormatter = (date) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return String.raw`${
+    months[date.getMonth()]
+  } ${date.getDay()}, ${date.getFullYear()}`.toString();
 };
 
 const CalendarPage = () => {
   const [currDate, onDateChange] = useState(new Date());
 
   return (
-    <div>
+    <div className={styles.bg}>
       <Calendar
         onChange={onDateChange}
         value={currDate}
         formatShortWeekday={dayLabelFormatter}
       />
+      <p className={styles.calendarHeader}>
+        {dailyTaskDateFormatter(currDate)}
+      </p>
     </div>
   );
 };
