93de4ec99446d01c5845419f8b3e71720db41f66
Vallens Kho <secure.mail2357@gmail.com>
2022-02-26 10:44:57 -0500

    Implement SmallActionButton styling

--- client/src/components/SmallActionButton/SmallActionButton.js
+++ client/src/components/SmallActionButton/SmallActionButton.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./SmallActionButton.module.css";
+import { PropTypes } from "prop-types";
 
-const SmallActionButton = (text) => {
-  return <div></div>;
+const SmallActionButton = ({ text, startEndClick }) => {
+  return (
+    <button className={styles.buttonStyle} onClick={startEndClick}>
+      {text}
+    </button>
+  );
+};
+
+SmallActionButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  startEndClick: PropTypes.func,
 };
 
 export default SmallActionButton;
