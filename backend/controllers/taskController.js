30ccd10c5bf3cf82af04ab7ea4075e8b6cef5dd7
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-02-26 13:25:40 -0500

    implemented and tested all task routes

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Task = require("../models/userTasks");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const e = require("express");
 
 const createTask = asyncHandler(async (req, res) => {
   // TODO:
@@ -16,54 +17,95 @@ const createTask = asyncHandler(async (req, res) => {
 });
 
 const getTasks = asyncHandler(async (req, res) => {
-  const userId = req.body.user_id; 
+  // TESTED
+  // console.log("in GetTasks");
+  const userId = req.body.user_id;
+  console.log(userId);
+  const tasks = await Task.find({ user_id: userId });
 
-  const tasks = await new Task.find({ user_id: userId });
-
-  console.log(tasks);
+  // console.log("TASKS:");
+  // console.log(tasks);
   res.status(200).json(tasks);
 });
 
 const getTasksById = asyncHandler(async (req, res) => {
   // Task id Parameter
-  const id = req.params.id; 
+  console.log("In getTasksById");
+  const id = req.params.id;
 
   // Users id
   const userId = req.body.user_id;
 
-  const tasks = new Task.find({
-    user_id: userId
-  }).findById(req.body.id);
-
+  const tasks = await Task.find({
+    user_id: userId,
+  });
+  // .findById(req.body.id);
   console.log(tasks);
-  res.status(200).json(tasks)
-  
-})
+  console.log(id);
+  const userTasks = tasks.filter(function (task) {
+    return task._id == id;
+  });
 
+  console.log("User Tasks:");
+  console.log(userTasks);
+  res.status(200).json(userTasks);
+});
 
 const getTasksByDay = asyncHandler(async (req, res) => {
-  // TODO: ?
-  // const date = req.params.day; 
+  // TODO: Extract the user_id from JWT instead
+  // of getting it from the body. GET does not take any body!
+  // After extracting user_id from JWT, pass it into object when
+  // we call Task.find()
 
-  // Users id
+  const date = req.params.day; // get a day -> 26
+  console.log("In getTasksByDay");
+
+  // Users id -> Extract from JWT since GET does not take any body
   const userId = req.body.user_id;
-  const startTime = req.body.startTime;
-  const endTime = req.body.endtime;
+  console.log("In getTasksByDay");
+  let startDate = 0;
+  let endDate = 0;
+  console.log("user_id:" + userId);
 
+  if (date.length === 8) {
+    // parse
+    const year = parseInt(date.substring(0, 4));
+    const month = parseInt(date.substring(4, 6));
+    const day = parseInt(date.substring(6, 8));
+    console.log("year:" + year);
+    console.log("month:" + month);
+    console.log("day:" + day);
 
-  const tasks = new Task.find({
+    try {
+      startDate = new Date(year, month - 1, day);
+      console.log(startDate);
+      endDate = new Date(year, month - 1, day + 2);
+      console.log(endDate);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Invalid Date Input");
+    }
+  }
+
+  // Check if its a valid date object
+  const aaa = new Date(2022, 1, 20);
+  const bbb = new Date("2022-02-20T15:02:08");
+  console.log("aaa:", aaa);
+  console.log("bbb:", bbb);
+
+  const tasks = await Task.find({
     user_id: userId,
-    startDate: { $gte: startTime ,$lte: endTime}
-  })
-
+    startDate: {
+      // $gte: startDate,
+      $lte: startDate,
+    },
+    endDate: {
+      $gte: startDate,
+    },
+    isStarted: true,
+  });
   console.log(tasks);
   res.status(200).json(tasks);
-  
-
-})
-
-
-
-
+});
 
 module.exports = { createTask, getTasks, getTasksByDay, getTasksById };
--- backend/models/userTasks.js
+++ backend/models/userTasks.js
@@ -1,38 +1,38 @@
 const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true
-        },
-        user_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        startDate: {
-            type: Date,
-            default: Date.now(),
-            required: true
-        },
-        endDate: {
-            type: Date,
-            required: true
-        },
-        isStarted: {
-            type: Boolean,
-            required: true
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    isStarted: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
-
+// console.log("Heloe");
 const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
