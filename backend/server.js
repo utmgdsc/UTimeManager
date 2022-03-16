f33291484f54fa6dcd849cb93fc823699a0f803a
Chris Lim <hello@hiimchrislim.co>
2022-03-16 01:35:13 -0400

    Temporarily remove main API

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,15 @@ app.use(notFound);
 app.use(errorHandler);
 
 let server;
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === "test") {
     server = app.listen();
     console.log("Running Test Server");
-} else {
-    connectDB();
-    server = app.listen(5000, console.log("Server running on port 5000"));
 }
+// else {
+//     connectDB();
+//     server = app.listen(5000, console.log("Server running on port 5000"));
+// }
 
 
 module.exports = {app, server}
