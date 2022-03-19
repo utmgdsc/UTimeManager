adaf2dd30a6ed84b248ec814d266dee482d56ed3
Vallens Kho <secure.mail2357@gmail.com>
2022-03-19 11:38:39 -0400

    Implement TaskHistoryPage

---
+++ client/src/pages/TaskHistoryPage/TaskHistoryPage.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styles from "./TaskHistoryPage.module.css";
+import TaskListView from "../../components/TaskListView/TaskListView.js";
+
+const TaskHistoryPage = () => {
+  const myTasks = [
+    {
+      title: "GDSC Meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: false,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+    {
+      title: "GDSC Meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: false,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+    {
+      title: "GDSC Meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: false,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+    {
+      title: "GDSC Meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: false,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+    {
+      title: "another meeting",
+      locationText: "DH2020",
+      startTimeText: "05:00PM",
+      endTimeText: "07:00PM",
+      isOngoing: true,
+    },
+  ];
+  return (
+    <div className={styles.bg}>
+      <div className={styles.taskHistoryHeader}>Your Tasks</div>
+      <TaskListView tasks={myTasks} taskEdittable={false} />
+    </div>
+  );
+};
+
+export default TaskHistoryPage;
