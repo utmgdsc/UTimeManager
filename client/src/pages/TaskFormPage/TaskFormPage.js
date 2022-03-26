009020cb1d14a25213145553fd494a15f4b0a1b1
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-03-26 17:46:44 -0400

    Resolve TaskFormPage PR

--- client/src/pages/TaskFormPage/TaskFormPage.js
+++ client/src/pages/TaskFormPage/TaskFormPage.js
@@ -2,25 +2,40 @@ import React from "react";
 import styles from "./TaskFormPage.module.css";
 import { InputBox } from "../../components/InputBox/InputBox";
 import { CredentialsButton } from "../../components/CredentialsButton/CredentialsButton.js";
-import CloseButton from "react-bootstrap/CloseButton";
 
 const TaskFormPage = () => {
   return (
     <div className={styles.bg}>
-      <CloseButton variant="white" />
-      <p className={styles.Header}>Create a Task</p>
-      <div className={styles.inputBox}>
-        <InputBox header="Task Title" type="text" placeholder="Task Name" />
-        <InputBox header="Location" type="text" placeholder="Location Name" />
-        <InputBox
-          header="Description"
-          type="text"
-          placeholder=""
-          expanded={true}
-        />
-      </div>
-      <div className={styles.button}>
-        <CredentialsButton text="Create" />
+      <div>
+        <button
+          type={"button"}
+          className={styles.closeButton}
+          aria-label={"Close"}
+        >
+          &times;
+        </button>
+        <p className={styles.header}>Create a Task</p>
+        <div className={styles.inputBox}>
+          <InputBox
+            header={"Task Title"}
+            type={"text"}
+            placeholder={"Task Name"}
+          />
+          <InputBox
+            header={"Location"}
+            type={"text"}
+            placeholder={"Location Name"}
+          />
+          <InputBox
+            header={"Description"}
+            type={"text"}
+            placeholder={""}
+            expanded={true}
+          />
+        </div>
+        <div className={styles.button}>
+          <CredentialsButton text={"Create"} />
+        </div>
       </div>
     </div>
   );
