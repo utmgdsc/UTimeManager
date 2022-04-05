49c341ab23738700ff0cbe80852ca81d38293abd
Chris Lim <hello@hiimchrislim.co>
2022-04-04 22:32:35 -0400

    Resolve git merge on authenticateToken.js

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-<<<<<<< HEAD
-=======
 
 const authenticateToken = asyncHandler(async (req, res, next) => {
   // Get the token from the auth header (where it would be stored)
@@ -33,39 +31,5 @@ const authenticateToken = asyncHandler(async (req, res, next) => {
     throw new Error("Not authorized, token failed");
   }
 });
->>>>>>> develop
 
-const authenticateToken = asyncHandler(async (req, res, next) => {
-  // Get the token from the auth header (where it would be stored)
-  let token;
-
-<<<<<<< HEAD
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    const authHeader = req.headers.authorization;
-    token = authHeader.split(" ")[1];
-  }
-
-  // Verify that there is a token
-  if (!token) {
-    // Bad request
-    // Code subject to change
-    res.status(401);
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.id = decoded._id;
-    next();
-  } catch (error) {
-    res.status(401);
-    throw new Error("Not authorized, token failed");
-  }
-});
-
-=======
->>>>>>> develop
 module.exports = { authenticateToken };
