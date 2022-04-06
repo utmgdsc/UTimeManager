9bd14ed0aaed7864847875d178f82bafaff2f12a
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-04-06 18:01:29 -0400

    removed unnecessary imports

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -1,12 +1,5 @@
 const asyncHandler = require("express-async-handler");
 const Feedback = require("../models/feedbackModel");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const e = require("express");
-const { mongo } = require("mongoose");
-const { isValidObjectId } = require("mongoose");
-const ObjectId = require("mongoose").Types.ObjectId;
-
 
 const createFeedback = asyncHandler(async (req, res) => {
   const feedback = await new Feedback(req.body);
@@ -18,6 +11,5 @@ const createFeedback = asyncHandler(async (req, res) => {
     throw new Error("Invalid Task Input");
   }
 });
-
 
 module.exports = { createFeedback };
