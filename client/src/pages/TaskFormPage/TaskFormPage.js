0abaee27db6477133f2d60c91e63cdbdbc5cc8ee
Vallens Kho <secure.mail2357@gmail.com>
2022-04-09 18:33:39 -0400

    Implement data binding between input and states

--- client/src/pages/TaskFormPage/TaskFormPage.js
+++ client/src/pages/TaskFormPage/TaskFormPage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TaskFormPage.module.css";
 import { InputBox } from "../../components/InputBox/InputBox";
 import { CredentialsButton } from "../../components/CredentialsButton/CredentialsButton.js";
 
 const TaskFormPage = () => {
+  // TODO : limit to 500 characters
+  const [taskFormData, setTaskFormData] = useState({
+    title: "",
+    user_id: "", // TODO : figure out where to get it from
+    location: "",
+    description: "",
+    startDate: new Date(), // TODO : change to use date selector
+    endDate: new Date(), // TODO : change to use date selector
+    isStarted: false,
+  });
+
+  const updateTaskFormData = (e, value) => {
+    const newTaskFormData = { ...taskFormData };
+    newTaskFormData[value] = e.target.value;
+    setTaskFormData(newTaskFormData);
+  };
+
+  const createTask = () => {
+    console.log(taskFormData);
+  };
+
   return (
     <div className={styles.bg}>
       <div>
@@ -20,21 +41,27 @@ const TaskFormPage = () => {
             header={"Task Title"}
             type={"text"}
             placeholder={"Task Name"}
+            onChange={(e) => updateTaskFormData(e, "title")}
+            value={taskFormData.title}
           />
           <InputBox
             header={"Location"}
             type={"text"}
             placeholder={"Location Name"}
+            onChange={(e) => updateTaskFormData(e, "location")}
+            value={taskFormData.location}
           />
           <InputBox
             header={"Description"}
             type={"text"}
             placeholder={""}
             expanded={true}
+            onChange={(e) => updateTaskFormData(e, "description")}
+            value={taskFormData.description}
           />
         </div>
         <div className={styles.button}>
-          <CredentialsButton text={"Create"} />
+          <CredentialsButton text={"Create"} authAction={createTask} />
         </div>
       </div>
     </div>
