45b3c3f9b2b1c6fa0ddca4377e24613432b1e185
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-04-16 17:12:54 -0400

    refactored feedback and task controller

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -3,17 +3,22 @@ const Feedback = require("../models/feedbackModel");
 
 const createFeedback = asyncHandler(async (req, res) => {
   const feedback = await new Feedback(req.body);
-
   const userId = req.id;
   feedback.user_id = userId;
 
-  const createdFeedback = await feedback.save();
-  res.status(201).json(createdFeedback);
-
-  if (!createdFeedback) {
-    res.status(400);
-    throw new Error("Invalid Task Input");
-  }
+  const createdFeedback = await feedback
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400);
+        throw new Error("Invalid Feedback Input");
+      }
+      res.status(500);
+      throw new Error("Unable to create feedback");
+    });
 });
 
 module.exports = { createFeedback };
