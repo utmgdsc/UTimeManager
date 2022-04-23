4d8617036aa93ef4c01b69a0215c4bc65e8540b5
Vallens Kho <secure.mail2357@gmail.com>
2022-04-23 18:56:49 -0400

    Implement TaskDetails (with styling)

--- client/src/components/TaskDetails/TaskDetails.js
+++ client/src/components/TaskDetails/TaskDetails.js
@@ -2,17 +2,40 @@ import React from "react";
 import styles from "./TaskDetails.module.css";
 import { PropTypes } from "prop-types";
 
-const TaskDetails = ({ closeModalHandler }) => {
+const TaskDetails = ({
+  closeModalHandler,
+  title,
+  location,
+  description,
+  startDateTimeString,
+  endDateTimeString,
+}) => {
   return (
     <div className={styles.modal}>
+      <div className={styles.overlay}></div>
       <div className={styles.modal}>
-        <div className={styles.overlay}></div>
-        <div className={styles.modal}>
-          <button className={styles.closeModal} onClick={closeModalHandler}>
-            close
-          </button>
-          <h1>Task Details</h1>
-          <p>... Task Description ...</p>
+        <button className={styles.closeModal} onClick={closeModalHandler}>
+          close
+        </button>
+        <div className={styles.taskDetailsHeader}>{title}</div>
+        <div className={styles.taskDetailContainer}>
+          <span className={styles.taskDetailsField}>{"Location: "}</span>
+          <span className={styles.taskDetail}>{location}</span>
+        </div>
+
+        <div className={styles.taskDetailContainer}>
+          <span className={styles.taskDetailsField}>{"Description: "}</span>
+          <span className={styles.taskDetail}>{description}</span>
+        </div>
+
+        <div className={styles.taskDetailContainer}>
+          <span className={styles.taskDetailsField}>{"From: "}</span>
+          <span className={styles.taskDetail}>{startDateTimeString}</span>
+        </div>
+
+        <div className={styles.taskDetailContainer}>
+          <span className={styles.taskDetailsField}>{"To: "}</span>
+          <span className={styles.taskDetail}>{endDateTimeString}</span>
         </div>
       </div>
     </div>
@@ -21,6 +44,11 @@ const TaskDetails = ({ closeModalHandler }) => {
 
 TaskDetails.propTypes = {
   closeModalHandler: PropTypes.func,
+  title: PropTypes.string,
+  location: PropTypes.string,
+  description: PropTypes.string,
+  startDateTimeString: PropTypes.string,
+  endDateTimeString: PropTypes.string,
 };
 
 export default TaskDetails;
