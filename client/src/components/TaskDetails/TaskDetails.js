86f0275a700b9e64cb73ddd36208e190a84f6950
Vallens Kho <secure.mail2357@gmail.com>
2022-04-23 23:16:40 -0400

    Minor clean up

--- client/src/components/TaskDetails/TaskDetails.js
+++ client/src/components/TaskDetails/TaskDetails.js
@@ -12,31 +12,28 @@ const TaskDetails = ({
 }) => {
   return (
     <div className={styles.modal}>
-      <div className={styles.overlay}></div>
-      <div className={styles.modal}>
-        <button className={styles.closeModal} onClick={closeModalHandler}>
-          close
-        </button>
-        <div className={styles.taskDetailsHeader}>{title}</div>
-        <div className={styles.taskDetailContainer}>
-          <span className={styles.taskDetailsField}>{"Location: "}</span>
-          <span className={styles.taskDetail}>{location}</span>
-        </div>
+      <button className={styles.closeModal} onClick={closeModalHandler}>
+        close
+      </button>
+      <div className={styles.taskDetailsHeader}>{title}</div>
+      <div className={styles.taskDetailContainer}>
+        <span className={styles.taskDetailsField}>{"Location: "}</span>
+        <span className={styles.taskDetail}>{location}</span>
+      </div>
 
-        <div className={styles.taskDetailContainer}>
-          <span className={styles.taskDetailsField}>{"Description: "}</span>
-          <span className={styles.taskDetail}>{description}</span>
-        </div>
+      <div className={styles.taskDetailContainer}>
+        <span className={styles.taskDetailsField}>{"Description: "}</span>
+        <span className={styles.taskDetail}>{description}</span>
+      </div>
 
-        <div className={styles.taskDetailContainer}>
-          <span className={styles.taskDetailsField}>{"From: "}</span>
-          <span className={styles.taskDetail}>{startDateTimeString}</span>
-        </div>
+      <div className={styles.taskDetailContainer}>
+        <span className={styles.taskDetailsField}>{"From: "}</span>
+        <span className={styles.taskDetail}>{startDateTimeString}</span>
+      </div>
 
-        <div className={styles.taskDetailContainer}>
-          <span className={styles.taskDetailsField}>{"To: "}</span>
-          <span className={styles.taskDetail}>{endDateTimeString}</span>
-        </div>
+      <div className={styles.taskDetailContainer}>
+        <span className={styles.taskDetailsField}>{"To: "}</span>
+        <span className={styles.taskDetail}>{endDateTimeString}</span>
       </div>
     </div>
   );
