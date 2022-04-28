6a6b0e8d3de80a51cbf3cddd5dfab7141f9077c8
Chris Lim <hello@hiimchrislim.co>
2022-04-28 01:48:31 -0400

    Remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/CredentialsPage/LoginPage.js";
 import SignUpPage from "./pages/CredentialsPage/SignUpPage.js";
 import PageNotFound from "./pages/PageNotFound/PageNotFound.js";
